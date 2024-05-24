469761b7628b33510e1d9574659b037b1adbc1ff
Yuichi-Kawata <hetianyouyi234@gmail.com>
2024-05-24 23:39:10 +0900

    Upload python_basic_kadai007.py

---
+++ python_basic_kadai007.py
@@ -0,0 +1,24 @@
+array = ["月曜日は晴れです", 
+         "火曜日は雨です",
+         "水曜日は晴れです",
+         "木曜日は晴れです",
+         "金曜日は曇りです",
+         "土曜日は曇りのち雨です",
+         "日曜日は雷雨です"
+         ]
+
+# for i in range(len(array)):
+#     print(array[i])
+
+dictionary = {
+    "mon" : "晴れ",
+    "tue" : "雨",
+    "wed" : "晴れ",
+    "thu" : "晴れ",
+    "fri" : "曇り",
+    "sat" : "曇りのち雨",
+    "sun" : "雷雨"
+}
+
+# for week, weather in dictionary.items():
+#     print(f"{week} : {weather}")
